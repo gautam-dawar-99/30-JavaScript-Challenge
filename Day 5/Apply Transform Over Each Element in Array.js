31aa5db189cc146a2ae4c1eca1461115cde3d17c
Gautam Dawar <157568087+gautam-dawar-99@users.noreply.github.com>
2024-10-11 10:43:08 +0530

    Day 5

---
+++ Day 5/Apply Transform Over Each Element in Array.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    let array = []
+    for (let i = 0; i < arr.length; i++) {
+        array[i] = fn(arr[i], i);
+    }
+    return array
+};
