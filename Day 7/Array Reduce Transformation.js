ad176ed3a40f99559d30aad24d3567286695d199
Gautam Dawar <157568087+gautam-dawar-99@users.noreply.github.com>
2024-10-11 10:44:05 +0530

    Day 7

---
+++ Day 7/Array Reduce Transformation.js
@@ -0,0 +1,14 @@
+// problem link : https://leetcode.com/problems/array-reduce-transformation/description/
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+    let ans = init;
+    for (let i = 0; i < nums.length; i++) {
+        ans = fn(ans,nums[i]);
+    }
+    return ans;
+};
